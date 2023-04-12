8a32aa2f39504875214b1844bd52e5b4b728df7a
ellyrose <elly_turnbull@hotmail.co.uk>
2023-04-12 15:41:10 +0100

    Fix error in final final

--- VehcileCalculatorIterations/VehiclePriceCalculatorFinalFinal.php
+++ VehcileCalculatorIterations/VehiclePriceCalculatorFinalFinal.php
@@ -14,57 +14,57 @@ class VehiclePriceCalculatorFinalFinal
     public function getPrice(): float
     {
         $price = $this->rrp;
-        $price -= $this->rrp * $this->getMotMultiplier();
-        $price -= $this->rrp * $this->getServiceMultiplier();
-        $price -= $this->rrp * $this->getDamageCheckMultiplier();
+        $price -= $this->getMotReduction();
+        $price -= $this->getServiceReduction();
+        $price -= $this->getDamageCheckReduction();
 
         return max($price, 0);
     }
 
-    private function getMotMultiplier(): float
+    private function getMotReduction(): float
     {
         $currentDate = new DateTimeImmutable();
 
         if ($currentDate > $this->lastMotDate->add(new DateInterval('P1Y'))) {
-            return 0.75;
+            return $this->rrp * 0.25;
         }
 
         if ($currentDate > $this->lastMotDate->add(new DateInterval('P6M'))) {
-            return 0.95;
+            return $this->rrp * 0.05;
         }
 
-        return 1;
+        return 0;
     }
 
-    private function getServiceMultiplier(): float
+    private function getServiceReduction(): float
     {
         $currentDate = new DateTimeImmutable();
 
         if ($currentDate > $this->lastServiceDate->add(new DateInterval('P3Y'))) {
-            return 0.7;
+            return $this->rrp * 0.3;
         }
 
         if ($currentDate > $this->lastServiceDate->add(new DateInterval('P1Y'))) {
-            return 0.9;
+            return $this->rrp * 0.1;
         }
 
         if ($currentDate > $this->lastServiceDate->add(new DateInterval('P6M'))) {
-            return 0.95;
+            return $this->rrp * 0.05;
         }
 
-        return 1;
+        return 0;
     }
 
-    private function getDamageCheckMultiplier(): float
+    private function getDamageCheckReduction(): float
     {
         if ($this->damageCheckResult === 'Red') {
-            return  0.1;
+            return $this->rrp * 0.9;
         }
 
         if ($this->damageCheckResult === 'Orange') {
-            return  0.5;
+            return $this->rrp * 0.5;
         }
 
-        return 1;
+        return 0;
     }
 }
